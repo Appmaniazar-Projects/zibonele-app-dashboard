f0277be82ea196503d028714537b8c239d73c239
Appmaniazar-Projects <appmaniazar@gmail.com>
2024-06-06 09:09:46 +0200

    Added a lineup page along with the and retrieved data from the database.

--- src/components/LineUp/Lineup.jsx
+++ src/components/LineUp/Lineup.jsx
@@ -1,10 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react';
 import './Lineup.css';
 
-function Lineup() {
-  return (
-    <div className="text-container">Lineup</div>
-  )
-}
+const Lineup = ({ schedule }) => {
+    const [selectedDay, setSelectedDay] = useState('mon');
 
-export default Lineup
+    const handleTabClick = (day) => {
+        setSelectedDay(day);
+    };
+
+    const daysOfWeek = {
+        mon: "Mon",
+        tue: "Tue",
+        wed: "Wed",
+        thu: "Thu",
+        fri: "Fri",
+        sat: "Sat",
+        sun: "Sun"
+    };
+
+    return (
+        <div className="lineup-container">
+            <div className="lineup-tabs">
+                {Object.keys(daysOfWeek).map((day) => (
+                    <div
+                        key={day}
+                        className={`lineup-tab-button ${selectedDay === day ? 'active' : ''}`}
+                        onClick={() => handleTabClick(day)}
+                    >
+                        {daysOfWeek[day]}
+                    </div>
+                ))}
+            </div>
+            <div className="lineup-content">
+                {Object.keys(schedule).map((day) => (
+                    <div
+                        key={day}
+                        id={day}
+                        className={`lineup-day-content ${selectedDay === day ? 'active' : ''}`}
+                    >
+                        {/* Render content only if the selected day matches the current iteration day */}
+                        {selectedDay === day && schedule[day].map((show, index) => (
+                            <div key={index} className="lineup-card">
+                                <p>{show.time}</p>
+                                <p>{show.title}</p>
+                                <p>{show.description}</p>
+                            </div>
+                        ))}
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Lineup;
