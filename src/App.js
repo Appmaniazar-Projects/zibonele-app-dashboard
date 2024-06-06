1b4076d8ad62f6dd4e83995f844c1938b0b49351
Appmaniazar-Projects <appmaniazar@gmail.com>
2024-06-06 09:11:12 +0200

    rendered the linup page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect} from 'react';
+import { database, ref, get } from './firebaseConfig';
 import './App.css';
 import Sidebar from './components/Sidebar/Sidebar';
 import Header from './components/Header/Header';
@@ -13,6 +14,39 @@ function App() {
     const [activeSection, setActiveSection] = useState('');
     const [isAddModalOpen, setAddModalOpen] = useState(false);
     const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [schedule, setSchedule] = useState({});
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        // Reference to the 'weeklySchedule' node in your Realtime Database
+        const scheduleRef = ref(database, 'weeklySchedule');
+
+        // Listen for changes to the data
+        const fetchData = async () => {
+            try {
+                const snapshot = await get(scheduleRef);
+                if (snapshot.exists()) {
+                    setSchedule(snapshot.val());
+                } else {
+                    console.log("No data available");
+                }
+                setLoading(false);
+            } catch (error) {
+                console.error("Error fetching data: ", error);
+            }
+        };
+
+        fetchData();
+
+        // Cleanup function to unsubscribe from the listener when component unmounts
+        return () => {
+            // You don't need to unsubscribe for Realtime Database
+        };
+    }, []);
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
 
     const handleLogin = () => {
         setIsLoggedIn(true);
@@ -26,7 +60,7 @@ function App() {
     const renderContent = () => {
         switch (activeSection) {
             case 'Shows Line Up':
-                return <Lineup />;
+                return <Lineup schedule={schedule} />;
             case 'Events':
                 return <Events />;
             case 'Dj Profiles':
@@ -43,7 +77,6 @@ function App() {
     if (!isLoggedIn) {
         return <Login onLogin={handleLogin} />;
     }
-
 
     return (
         <div className="container">
