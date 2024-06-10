2bcd3cb3b7aa55fddb88a51cb4d2e0fe5b240919
Appmaniazar-Projects <appmaniazar@gmail.com>
2024-06-10 15:49:12 +0200

    Removed sidebar link

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -14,7 +14,7 @@ function Sidebar({ setActiveSection, onLogout }) {
           <li onClick={() => setActiveSection('Shows Line Up')}><i className="fas fa-list"></i> Shows Line Up</li>
           <li onClick={() => setActiveSection('Events')}><i className="fas fa-calendar-alt"></i> Events</li>
           <li onClick={() => setActiveSection('Dj Profiles')}><i className="fas fa-user"></i> Dj Profiles</li>
-          <li onClick={() => setActiveSection('Social Media')}><i className="fas fa-share-alt"></i> Social Media</li>
+          {/* <li onClick={() => setActiveSection('Social Media')}><i className="fas fa-share-alt"></i> Social Media</li> */}
           <li onClick={() => setActiveSection('Settings')}><i className="fas fa-cog"></i> Settings</li>
           <li  onClick={onLogout}><i className="fas fa-sign-out-alt"></i> Logout</li>
         </ul>
