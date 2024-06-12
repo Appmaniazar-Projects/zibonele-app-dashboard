e96c0b91e32b55c2eb172408a38c6a2cc23920e3
Appmaniazar-Projects <appmaniazar@gmail.com>
2024-06-12 11:58:00 +0200

    updated header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,20 @@
-import React from 'react';
+// src/components/Header/Header.js
+import React, { useContext } from 'react';
+import { ProfileContext } from '../../ProfileContext'; // Import the context
 import './Header.css';
 
 function Header() {
-  return (
-    <header>
-      <h1>Welcome Back, </h1>
-      <div className="user-icon">
-        <img src="https://static.vecteezy.com/system/resources/previews/019/879/186/non_2x/user-icon-on-transparent-background-free-png.png" alt="User Icon" />
-      </div>
-    </header>
-  );
+    const { profilePictureURL, userName } = useContext(ProfileContext); // Use the context
+
+    return (
+        <header>
+            <h1>Welcome , {userName}</h1>
+            
+            <div className="user-icon">
+                <img src={profilePictureURL || "https://static.vecteezy.com/system/resources/previews/019/879/186/non_2x/user-icon-on-transparent-background-free-png.png"} alt="User Icon" />
+            </div>
+        </header>
+    );
 }
 
 export default Header;
